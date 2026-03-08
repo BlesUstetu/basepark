3b762d6339d55f0781ea8d26a2580a5ed47ddb80
BlesUstetu <lionustetu@gmail.com>
2026-03-08 16:46:20 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -16,27 +16,17 @@ const queryClient = new QueryClient()
 const config = getDefaultConfig({
   appName: "BaseParkVault",
   projectId: "0xd2f9411079a3362d3e20cef1719cf2d8a3923d8d",
-  chains: [base]
+  chains: [base],
 })
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-
   <React.StrictMode>
-
     <QueryClientProvider client={queryClient}>
-
       <WagmiProvider config={config}>
-
-        <RainbowKitProvider>
-
+        <RainbowKitProvider chains={[base]}>
           <App />
-
         </RainbowKitProvider>
-
       </WagmiProvider>
-
     </QueryClientProvider>
-
   </React.StrictMode>
-
 )
