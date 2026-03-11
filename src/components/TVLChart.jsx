776324c72d4a0ee4d2f9df242147244e6c372512
BlesUstetu <lionustetu@gmail.com>
2026-03-11 11:06:45 +0200

    Update TVLChart.jsx

--- src/components/TVLChart.jsx
+++ src/components/TVLChart.jsx
@@ -1,20 +1,50 @@
+import {
+LineChart,
+Line,
+XAxis,
+YAxis,
+Tooltip,
+ResponsiveContainer
+} from "recharts"
 
-import { LineChart,Line,XAxis,YAxis,Tooltip } from "recharts"
+const data=[
 
-export default function TVLChart({data}){
+{time:"10:40",tvl:120},
+{time:"10:41",tvl:150},
+{time:"10:42",tvl:130},
+{time:"10:43",tvl:170},
+{time:"10:44",tvl:160}
+
+]
+
+export default function TVLChart(){
 
 return(
 
 <div className="neon-card">
 
-<h2>TVL Chart</h2>
+<h2>Total Value Locked</h2>
 
-<LineChart width={500} height={250} data={data}>
+<ResponsiveContainer width="100%" height={260}>
+
+<LineChart data={data}>
+
 <XAxis dataKey="time"/>
+
 <YAxis/>
+
 <Tooltip/>
-<Line type="monotone" dataKey="tvl"/>
+
+<Line
+type="monotone"
+dataKey="tvl"
+stroke="#00ffd5"
+strokeWidth={3}
+/>
+
 </LineChart>
+
+</ResponsiveContainer>
 
 </div>
 
