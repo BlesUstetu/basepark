3f8b6eaf0bcaeacf34db516bf3a2a7154e738906
BlesUstetu <lionustetu@gmail.com>
2026-03-11 11:00:24 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,42 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
+import "./index.css"
 
-import "@rainbow-me/rainbowkit/styles.css";
-import { getDefaultConfig, RainbowKitProvider, darkTheme } from "@rainbow-me/rainbowkit";
-import { WagmiProvider } from "wagmi";
-import { base } from "wagmi/chains";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "@rainbow-me/rainbowkit/styles.css"
 
-const queryClient = new QueryClient();
+import {
+getDefaultConfig,
+RainbowKitProvider
+} from "@rainbow-me/rainbowkit"
+
+import { WagmiProvider } from "wagmi"
+import { base } from "wagmi/chains"
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query"
 
 const config = getDefaultConfig({
-  appName: "BaseParkVault",
-  projectId: "766cbd364adce0a339344216092628d5", // <--- GANTI INI
-  chains: [base],
-  ssr: true,
-});
+appName:"BaseParkVault",
+projectId:"baseparkvault",
+chains:[base],
+ssr:false
+})
+
+const queryClient=new QueryClient()
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider theme={darkTheme({ accentColor: '#00ffcc', accentColorForeground: 'black' })}>
-          <App />
-        </RainbowKitProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
-  </React.StrictMode>
-);
+
+<WagmiProvider config={config}>
+
+<QueryClientProvider client={queryClient}>
+
+<RainbowKitProvider>
+
+<App/>
+
+</RainbowKitProvider>
+
+</QueryClientProvider>
+
+</WagmiProvider>
+
+)
