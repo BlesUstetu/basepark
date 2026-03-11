48031e2939a0474a9c721d3b67471592bbce291f
BlesUstetu <lionustetu@gmail.com>
2026-03-11 11:03:57 +0200

    Create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit"
+
+export default function Header(){
+
+return(
+
+<header className="header">
+
+<div className="logo">
+BASEPARK <span>VAULT</span>
+</div>
+
+<ConnectButton/>
+
+</header>
+
+)
+
+}
